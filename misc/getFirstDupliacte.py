cb5c945fa954d701df90295d330e35f2fc7b6141
sr-vishnu <vishnu754933@gmail.com>
2022-03-29 11:34:15 +0530

    fix

--- misc/getFirstDupliacte.py
+++ misc/getFirstDupliacte.py
@@ -1,4 +1,14 @@
 
 
 def getFirstDuplicate(array):
-    counterArray = [0 for _ in range(len(array))]
+    counterArray = [0 for _ in range(len(array)+1)]
+
+    for element in array:
+        elementCount = counterArray[element]
+
+        if(elementCount <= 1):
+            counterArray[element] = element+1
+        else:
+            return element
+
+    return -1
